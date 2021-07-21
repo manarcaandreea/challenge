a5bb8b340727d4052afa9497c993bf99f7d0c828
Manarca Andreea <a.manarca@yahoo.com>
2021-07-21 18:55:20 +0300

    Add project files.

---
+++ WebForm1.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="challenge.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Generate one-time password:
+                <asp:Button ID="Button1" runat="server" Text="Generate" OnClick="Button1_Click" /><br />
+                <asp:Label ID="11" runat="server" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace challenge
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string num = "0123456789";
+            int len = num.Length;
+            string otp = string.Empty;
+            int otpdigit = 5;
+            string finaldigit;
+            int getindex;
+            for (int i = 0; i < otpdigit; i++)
+            {
+                do
+                {
+                    getindex = new Random().Next(0, len);
+                    finaldigit = num.ToCharArray()[getindex].ToString();
+
+                } while (otp.IndexOf(finaldigit) != -1);
+                otp += finaldigit;
+            }
+            11.Text = otp;
+        }
+    }
+}
